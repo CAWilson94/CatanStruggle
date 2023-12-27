b424dcd345847baf9ced92418312d90d3cb8c624
CAWilson94 <charlotteawilson.94@gmail.com>
2023-12-27 22:39:19 +0100

    backgrounds

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,9 +7,9 @@ export interface CardProps {
 const Card = (props: CardProps) => {
   const { points, description, imagePath } = props;
   return (
-    <div className="w-full rounded overflow-hidden shadow-lg">
-     <img className="w-40 h-48 object-cover" src={imagePath} alt="sheep.jpeg" />
-    <div className="px-6 py-4">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+     <img className="rounded-t-lg w-40 h-48 object-cover" src={imagePath} alt="sheep.jpeg" />
+    <div className="px-6 py-4 rounded-lg bg-white">
       <div className="font-bold text-xl mb-2">{description}</div>
       <p className="text-gray-700 text-base"> 
         {`${points} Points`}
