721667748aed54e0a9d26d1837e25c25e8565bc0
CAWilson94 <charlotteawilson.94@gmail.com>
2023-12-28 10:51:22 +0100

    knights and roads out of market

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,15 @@
 export interface CardProps {
-  points: number;
   description: string;
   imagePath: string | undefined;
 }
 
 const Card = (props: CardProps) => {
-  const { points, description, imagePath } = props;
+  const { description, imagePath } = props;
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm ml-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
      <img className="rounded-t-lg w-40 h-48 object-cover" src={imagePath} alt="sheep.jpeg" />
     <div className="px-6 py-4 rounded-lg bg-white">
       <div className="font-bold text-xl mb-2">{description}</div>
-      <p className="text-gray-700 text-base"> 
-        {`${points} Points`}
-      </p>
     </div>
   </div>
   );
