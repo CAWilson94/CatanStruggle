8feeabc79f11730a3e469c495b2031c9608c27bc
CAWilson94 <charlotteawilson.94@gmail.com>
2023-12-29 10:48:42 +0100

    discard pile on screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,29 @@ import { shuffleListItems } from "@/utils/utils";
 import { useState } from "react";
 
 export default function Home() {
-
-  const resourceBaseGameCards: ResourceDecks = marketBaseDeck(baseResourceCards);
-  
-  const [resourceCardStack, setResourceCardStack] = useState(() =>
-    shuffleListItems(resourceBaseGameCards.mainDeck)
+  const [resourceCardStack, setResourceCardStack] = useState(
+    marketBaseDeck(baseResourceCards)
   );
 
   const shuffleCards = () => {
-    setResourceCardStack(shuffleListItems(resourceBaseGameCards.mainDeck));
+    let currentResources: ResourceDecks = {
+      mainDeck: shuffleListItems(resourceCardStack.mainDeck),
+      discardPile: resourceCardStack.market,
+      market: resourceCardStack.mainDeck.splice(0, 5),
+    };
+    setResourceCardStack(currentResources);
   };
-
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row">
-        <div className="flex flex-col">
+      <div className="flex flex-row gap-2">
+        <div className="flex flex-col gap-2">
           <div>
-            <h1 className="px-6 font-bold text-xl mb-2">Market</h1>
+            <h1 className="px-6 font-bold text-xl mb-2 ml-1">Market</h1>
           </div>
           <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
             <div className="card-list">
-              {resourceBaseGameCards.market.map((card, index) => (
+              {resourceCardStack.market.map((card, index) => (
                 <Card description={card as string} imagePath={`${card}.jpeg`} />
               ))}
               <style jsx>{`
@@ -53,12 +53,36 @@ export default function Home() {
               Refresh Cards
             </button>
           </div>
+          <div>
+            <div className="discard-pile">
+              <Card
+                description={resourceCardStack.discardPile[0] as string}
+                imagePath={`${resourceCardStack.discardPile[0]}.jpeg`}
+              />
+              <style jsx>{`
+                .discard-pile {
+                  display: flex;
+                  flex-wrap: wrap;
+                  justify-content: start;
+                  padding: 16px;
+                }
+              `}</style>
+            </div>
+            <h1 className="px-6 font-bold text-xl mb-2 ml-1">Discard Pile</h1>
+          </div>
         </div>
-        <div>
-          <Card description={resourceBaseGameCards.mainDeck[0] as string} imagePath={`${resourceBaseGameCards.mainDeck[0]}.jpeg`} />
+        <div className="main-deck">
+          <Card
+            description={resourceCardStack.mainDeck[0] as string}
+            imagePath={`${resourceCardStack.mainDeck[0]}.jpeg`}
+          />
+          <style jsx>{`
+            .main-deck {
+              padding-top: 60px;
+            }
+          `}</style>
         </div>
       </div>
     </main>
   );
 }
-
