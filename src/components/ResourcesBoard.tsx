0d7b28ba34699d9effa2c0a2707c92aa2ce319f2
CAWilson94 <charlotteawilson.94@gmail.com>
2023-12-30 20:49:16 +0100

    resources board component

---
+++ src/components/ResourcesBoard.tsx
@@ -0,0 +1,96 @@
+import {
+  ResourceDecks,
+  baseResourceCards,
+  marketBaseDeck,
+} from "@/modules/Resources/Resources";
+import { shuffleListItems } from "@/utils/utils";
+import React, { useState } from "react";
+import Card from "./Card";
+
+const ResourcesBoard = () => {
+  const initialMarket: string[] = baseResourceCards;
+
+  const [resourceCardStack, setResourceCardStack] = useState<ResourceDecks>(
+    marketBaseDeck(initialMarket)
+  );
+
+  const shuffleCards = () => {
+    const shuffledMainDeck = shuffleListItems(resourceCardStack.mainDeck);
+    const newMarket = shuffledMainDeck.slice(0, 5);
+    const updatedMainDeck = shuffledMainDeck.slice(5);
+
+    const currentResources: ResourceDecks = {
+      mainDeck: updatedMainDeck,
+      discardPile: [
+        ...resourceCardStack.discardPile,
+        ...resourceCardStack.market,
+      ],
+      market: newMarket,
+    };
+    setResourceCardStack(currentResources);
+  };
+
+  return (
+    <div className="flex flex-row gap-2">
+      <div className="flex flex-col gap-2">
+        <div>
+          <h1 className="px-6 font-bold text-xl mb-2 ml-1">Market</h1>
+        </div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <div className="card-list">
+            {resourceCardStack.market.map((card, index) => (
+              <Card description={card as string} imagePath={`${card}.jpeg`} />
+            ))}
+            <style jsx>{`
+              .card-list {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-around;
+                padding: 16px; // Adjust as needed
+              }
+            `}</style>
+          </div>
+        </div>
+        <div className="px-6">
+          <button
+            className="w-40 mt-4 p-4 py-2 bg-blue-500 text-white rounded"
+            onClick={shuffleCards}
+          >
+            Refresh Market
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <div>
+          <div className="discard-pile">
+            <Card
+              description={resourceCardStack.discardPile[0] as string}
+              imagePath={`${resourceCardStack.discardPile[0]}.jpeg`}
+            />
+            <style jsx>{`
+              .discard-pile {
+                padding: 16px;
+                padding-top: 60px;
+              }
+            `}</style>
+            <h1 className="px-4 pt-6 font-bold text-xl mb-2 ml-1">Discard</h1>
+          </div>
+        </div>
+        <div className="main-deck">
+          <Card
+            description={resourceCardStack.mainDeck[0] as string}
+            imagePath={`${resourceCardStack.mainDeck[0]}.jpeg`}
+          />
+          <style jsx>{`
+            .main-deck {
+              padding-top: 60px;
+            }
+          `}</style>
+          <h1 className="px-4 pt-6 font-bold text-xl mb-2 ml-1">Draw</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResourcesBoard;
